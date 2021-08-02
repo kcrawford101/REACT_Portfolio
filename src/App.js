d6e4b4ccff15c7c09c2c76042bf5e7d630e4cbae
Kevin Crawford <crawfordkevin101@gmail.com>
2021-08-02 02:43:18 -0400

    added some styling changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import displaypic from './images/My_Display_Pic_25.jpg'
 
 import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
@@ -7,19 +8,26 @@ import Footer from './Components/Footer/Footer';
 import Projects from './Components/Projects/Projects';
 
 
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      
       <div className= 'page-body'>
-        <h2 >About Me</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        
-        <div className='project-container'>
 
-        <h2 className='mb-4'>Projects</h2>
-        <Projects />
+        
+        <h2 style={{textAlign: 'start', marginLeft: '20px'}} >About Me</h2>
+        
+        <div className='about-me'>        
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        
+        <h2 className='mb-4' style={{textAlign: 'start', marginLeft: '20px'}}>Projects</h2>
+        <div className='project-container'>
+          
+          <Projects />
         </div>     
         <Footer />
       </div>     
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import heroimg from '../../images/hero.png';
+import heroimg from '../../images/hero.JPG';
 
 
 const Hero = () => {
     return (
         <div>
-            <img src={heroimg} fluid ></img>
+            <img style={{marginBottom: '20px', width: '100%'}} src={heroimg} fluid ></img>
         </div>        
     )
 }
